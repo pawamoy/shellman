d6fcc8a322001b2d800a83fa75355a982ae5cc0f
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-02 17:58:54 +0200

    Fix version in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ project = u'Shellman'
 year = '2017'
 author = u'Timothee Mazzucotelli'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.1.0'
+version = release = u'0.2.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
