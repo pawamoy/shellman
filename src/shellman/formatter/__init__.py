87e92df8b9577968db090ac00bada17086368574
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-05 10:46:05 +0200

    Remove MPL2.0 notices in sources (now ISC)

--- src/shellman/formatter/__init__.py
+++ src/shellman/formatter/__init__.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Timothée Mazzucotelli
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 """
 Formatter module.
 
