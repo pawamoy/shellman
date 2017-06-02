ce23c1d983851d24199cd5113d143b353fda569a
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-06-02 14:35:19 +0200

    Readd __version__ to fix cli

--- src/shellman/__init__.py
+++ src/shellman/__init__.py
@@ -10,3 +10,5 @@ brief, desc, fn, author, so you can write documentation in your shell scripts.
 After having retrieved the documentation comments, shellman will be able to
 write this documentation as text, man, or markdown format on stdout.
 """
+
+__version__ = '0.2.2'
